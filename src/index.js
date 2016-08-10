8c9b7e2856bbefdea4ad63f1bd6cd018c74a4602
cocoon-break <13821092033@163.com>
2016-08-10 13:10:19 +0800

    tab multi usage

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {
     Text,
     Dimensions,
     TouchableHighlight,
-    View
+    View,
+    Image
 } from 'react-native';
 const screen = Dimensions.get('window')
 const screenHeight = screen.height
@@ -29,11 +30,18 @@ class TableView extends Component {
 
 TableView.TableRow = (props)=> {
     const isPressAble = props.onPress ? true : false
+    const showArrow = props.showArrow ? true : false
+    const rightIconSource = props.rightIconSource ? props.rightIconSource : require('./list_arrow.png')
+    const subElement = props.subElement ? props.subElement :
+        <Text style={[styles.subtitleStyle,props.subtitleStyle]}>{props.subtitle}</Text>
+    const titleElement = props.titleElement ? props.titleElement :
+        <Text style={[styles.titleStyle,props.titleStyle]}>{props.title}</Text>
 
     let renderCell = ()=> {
         let cell = <View style={[styles.cellStyle,props.style]}>
-            <Text style={[styles.titleStyle,props.titleStyle]}>{props.title}</Text>
-            <Text style={[styles.subtitleStyle,props.subtitleStyle]}>{props.subtitle}</Text>
+            {titleElement}
+            {subElement}
+            {showArrow ? <Image source={rightIconSource} style={{marginLeft:4}}></Image> : null}
         </View>
         cell = isPressAble ? <TouchableHighlight onPress={props.onPress}
                                                  underlayColor={props.underlayColor}>{cell}</TouchableHighlight> : cell
@@ -52,15 +60,15 @@ const styles = StyleSheet.create({
         padding: 12,
         width: screenWidth,
         backgroundColor: 'white',
-        borderBottomColor:'#ccc',
-        borderBottomWidth:StyleSheet.hairlineWidth
+        borderBottomColor: '#ccc',
+        borderBottomWidth: StyleSheet.hairlineWidth
     },
-    titleStyle:{
-        fontSize:16,
-        flex:1
+    titleStyle: {
+        fontSize: 16,
+        flex: 1
     },
-    subtitleStyle:{
-        fontSize:14
+    subtitleStyle: {
+        fontSize: 14
     }
 })
 
